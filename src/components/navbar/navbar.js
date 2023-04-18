89ce3fd28ae3c82aa0ee556c59342783e5d11bbe
arindam <arindamroy1002@gmail.com>
2023-04-18 23:46:23 +0530

    commit

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import './navbar.css'
+function NavBar() {
+  return (
+    <header>
+      <h2 className="title"><a className="title-link" href="../public/index.html">Crypto Price Checker</a></h2>
+    </header>
+  );
+}
+
+export default NavBar;
